80bc94e0bb891e95f14b32030e6da8f3bd0cdc84
Rohan Yadav <yadavrohan98040@gmail.com>
2025-03-29 17:31:44 +0530

    add:program to find sum of digits in a given number

---
+++ SumOfDigits.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,r,sum=0;
+    printf("Enter any Number: ");
+    scanf("%d",&n);
+    while(n>0){
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    printf("Sum of Digits: %d ",sum);
+}
